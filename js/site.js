aa9aadf46fe6ea792bbbf92822319ceec0711275
RedAngelRedux <sammy@sammynava.com>
2023-02-13 12:47:14 -0500

    Created Pseudocode
    Sketched out pseudocode in site.js for the fizzbuzz app.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,30 @@
-// Call HelloWorld
-function HelloWorld() {
-    alert("Hello World");
+// CONTROLLER CONCERN
+function getValues() {
+
+    // obtain and validate fizzbuzz numbers
+
+    // obtain and validate range numbers
+
+    // invoke generateResults()
+
+    // invoke displayResults()
+
+}
+
+// LOGIC CONCERN
+function generateResults(fizz, buzz, start, end) {
+
+    // loop through range
+        // build array with each index containing the number, fizz, buzz, or fizzbuzz
+
+    // return array
+
+}
+
+// UI CONCERN
+function displayResults(results) {
+
+    // loop through array, creating html
+        // start new tr afer every 5 td's
+
 }
